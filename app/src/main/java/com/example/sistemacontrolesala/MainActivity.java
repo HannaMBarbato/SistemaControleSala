acd276675cacd2555d432168504d04e290217d35
Hanna MB <hanna_mb8@hotmail.com>
2020-01-28 16:59:35 -0300

    login usuario

--- app/src/main/java/com/example/sistemacontrolesala/MainActivity.java
+++ app/src/main/java/com/example/sistemacontrolesala/MainActivity.java
@@ -3,13 +3,22 @@ package com.example.sistemacontrolesala;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.sistemacontrolesala.listaSalas.ListaSalasView;
 import com.example.sistemacontrolesala.usuario.CadastroUsuario;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,14 +30,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final EditText editEmail = findViewById(R.id.editEmail);
+        final EditText editPassword = findViewById(R.id.editSenha);
+
         btnEntrar = findViewById(R.id.btnEntrar);
+
         btnEntrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, ListaSalasView.class));
-                finish();
+                String resultAuth = null;
+                final String email = editEmail.getText().toString();
+                final String password = editPassword.getText().toString();
+                try {
+                    resultAuth = new Verificador().execute(email, password).get();
+                    if (resultAuth.equals("Login efetuado com sucesso!")) {
+                        startActivity(new Intent(MainActivity.this, ListaSalasView.class));
+                        finish();
+                    }else {
+                        Toast.makeText(MainActivity.this, "errooou", Toast.LENGTH_SHORT).show();
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
+
         txtCadastrar = findViewById(R.id.txtCadastrarMain);
         txtCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,7 +64,34 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
 
+    public class Verificador extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            String urlWS = "http://172.30.248.117:8080/ReservaDeSala/rest/usuario/login/";
 
+            StringBuilder result = new StringBuilder();
+            try {
+                URL url = new URL(urlWS);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.setRequestProperty("authorization", "secret");
+                conn.setRequestProperty("email", strings[0]);
+                conn.setRequestProperty("password", strings[1]);
+                conn.setConnectTimeout(2000);
+
+                BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String line;
+                while ((line = rd.readLine()) != null) {
+                    result.append(line);
+                }
+                rd.close();
+                return result.toString();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return result.toString();
+        }
     }
 }
--- app/src/main/java/com/example/sistemacontrolesala/usuario/Usuario.java
+++ app/src/main/java/com/example/sistemacontrolesala/usuario/Usuario.java
@@ -1,4 +1,50 @@
 package com.example.sistemacontrolesala.usuario;
 
 public class Usuario {
+
+    private int id;
+    private int idOrganizacao;
+    private String nome;
+    private String email;
+    private String senha;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdOrganizacao() {
+        return idOrganizacao;
+    }
+
+    public void setIdOrganizacao(int idOrganizacao) {
+        this.idOrganizacao = idOrganizacao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
